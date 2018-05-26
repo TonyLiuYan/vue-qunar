f073b64f2810abe77ee227c897cf6360d8b23abd
345006662@qq.com <ly33854950>
2018-05-27 07:40:38 +0800

    icon样式

--- project/Travel/src/pages/home/Home.vue
+++ project/Travel/src/pages/home/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <home-header/>
     <home-swiper/>
-    test
+    <home-icons/>
   </div>
 </template>
 
 <script>
   import HomeHeader from './components/Header'
   import HomeSwiper from './components/Swiper'
+  import HomeIcons from './components/Icons'
+
   export default {
     name: 'Home',
     components: {
       HomeHeader,
-      HomeSwiper
+      HomeSwiper,
+      HomeIcons
     }
   }
 </script>
---
+++ project/Travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeIcons'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .icons
+    overflow :hidden
+    height :0
+    padding-bottom :50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+</style>
