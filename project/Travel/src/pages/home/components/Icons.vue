663106d23bd33db3f710744ee9e152e03d53125f
TonyLiu <345006662@qq.com>
2018-05-27 08:26:55 +0800

    图标区逻辑的实现

--- project/Travel/src/pages/home/components/Icons.vue
+++ project/Travel/src/pages/home/components/Icons.vue
@@ -1,52 +1,120 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'HomeIcons'
+    name: 'HomeIcons',
+    data: function() {
+      return {
+        iconList: [{
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '热门景点'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+            desc: '游乐场'
+          },
+          {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+            desc: '水上乐园'
+          },
+          {
+            id: '0004',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+            desc: '城市观光'
+          },
+          {
+            id: '0005',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+            desc: '一日游'
+          },
+          {
+            id: '0006',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+            desc: '动植物园'
+          },
+          {
+            id: '0007',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/27/dac2bcf9added702.png',
+            desc: '海滨海岛'
+          },
+          {
+            id: '0008',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+            desc: '海洋馆'
+          },
+          {
+            id: '0009',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+            desc: '旅游城'
+          }
+        ]
+      }
+    },
+    computed: {
+      pages() {
+        const pages = []
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        return pages
+      }
+    }
   }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .icons
-    overflow :hidden
-    height :0
-    padding-bottom :50%
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTextColor
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
 </style>
