ec2a34f9fc569f3c02d37a1ce180801dc35b7a95
TonyLiu <345006662@qq.com>
2018-05-27 16:54:34 +0800

    热销推荐

--- project/Travel/src/pages/home/Home.vue
+++ project/Travel/src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header/>
     <home-swiper/>
     <home-icons/>
+    <home-recommend/>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
   import HomeHeader from './components/Header'
   import HomeSwiper from './components/Swiper'
   import HomeIcons from './components/Icons'
+  import HomeRecommend from './components/Recommend'
 
   export default {
     name: 'Home',
     components: {
       HomeHeader,
       HomeSwiper,
-      HomeIcons
+      HomeIcons,
+      HomeRecommend
     }
   }
 </script>
---
+++ project/Travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item" v-for="item in recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeRecommend',
+    data() {
+      return {
+        recommendList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201307/15/7eab0785f2aa37e8c8d65eac.jpg_200x200_ca7ce0e0.jpg',
+          title: '大梅沙',
+          desc: '这里是介绍部分啊'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201307/15/7eab0785f2aa37e8c8d65eac.jpg_200x200_ca7ce0e0.jpg',
+          title: '小梅沙',
+          desc: '这里是介绍部分啊'
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
