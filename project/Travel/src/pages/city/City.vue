19f96447c555133e4da35210c804cf68204bd4d7
liuyan <ithk-test7@htsc.com>
2018-05-29 17:58:19 +0800

    字母选择联动功能

--- project/Travel/src/pages/city/City.vue
+++ project/Travel/src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -13,7 +13,6 @@ import CityHeader from "./components/Header";
 import CitySearch from "./components/Search";
 import CityList from "./components/List";
 import CityAlphabet from "./components/Alphabet";
-
 export default {
   name: "City",
   components: {
@@ -22,11 +21,12 @@ export default {
     CityList,
     CityAlphabet
   },
-  data () {
+  data() {
     return {
       cities: {},
-      hotCities: []
-    }
+      hotCities: [],
+      letter: ""
+    };
   },
   methods: {
     getCityInfo() {
@@ -39,6 +39,9 @@ export default {
         this.cities = data.cities;
         this.hotCities = data.hotCities;
       }
+    },
+    handleLetterChange(letter) {
+      this.letter = letter;
     }
   },
   mounted() {
--- project/Travel/src/pages/city/components/List.vue
+++ project/Travel/src/pages/city/components/List.vue
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item, key) of cities" :key="key">
+            <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
@@ -35,10 +35,19 @@ export default {
   name: "CityList",
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted() {
     this.scroll = new Bscroll(this.$refs.wrapper);
+  },
+  watch: {
+    letter() {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0];
+        this.scroll.scrollToElement(element);
+      }
+    }
   }
 };
 </script>
