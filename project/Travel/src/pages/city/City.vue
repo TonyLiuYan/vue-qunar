50a1b6b29b99d7e38fd95bb84f6acec26d6c7688
liuyan <ithk-test7@htsc.com>
2018-05-29 10:37:31 +0800

    城市列表搜索框布局

--- project/Travel/src/pages/city/City.vue
+++ project/Travel/src/pages/city/City.vue
@@ -1,15 +1,19 @@
 <template>
-    <div>
-        <city-header></city-header>
-    </div>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
+
 export default {
   name: "City",
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 };
 </script>
---
+++ project/Travel/src/pages/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.search {
+    height: 0.72rem;
+    padding: 0 0.1rem;
+    background: $bgColor;
+
+    .search-input {
+        box-sizing: border-box;
+        width: 100%;
+        height: 0.62rem;
+        padding: 0 0.1rem;
+        line-height: 0.62rem;
+        text-align: center;
+        border-radius: 0.06rem;
+        color: #666;
+    }
+}
+</style>
