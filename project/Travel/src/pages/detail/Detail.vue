29e4147cafdfae1e5e786663656fad3184f602d7
liuyan <ithk-test7@htsc.com>
2018-05-30 15:55:59 +0800

    Header渐隐渐现

--- project/Travel/src/pages/detail/Detail.vue
+++ project/Travel/src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height: 50rem
 </style>
